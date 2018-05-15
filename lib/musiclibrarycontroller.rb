bf3623314959e3ceac30f5391a2d9541851fa5fc
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-15 23:18:06 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -23,19 +23,10 @@ class MusicLibraryController
         gets
     end
 
+    def list_songs
+      Songs.all.each_with_index {|song, index| puts "#{index+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
+    end
 
-
-    case input
-      when input == "list songs"
-        Songs.all.each_with_index {|song, index| puts "#{index+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
-
-
-      end
-
-
-
+    end
   end
-
-
-
 end
