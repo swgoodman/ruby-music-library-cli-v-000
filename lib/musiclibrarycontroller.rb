99fe87f150105b5463f58e06c5772a2f4e7a1a3d
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-15 22:59:38 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -3,7 +3,7 @@ class MusicLibraryController
 
   attr_accessor :import_time
 
-  def initialize(path="")
+  def initialize(path="./db/mp3s/")
     @import_time = MusicImporter.new(path)
     @import_time.import
   end
