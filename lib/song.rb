0d579ab1fa513586af7636a1e0e3580f8ef00d45
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-15 22:03:34 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,11 +49,7 @@ class Song
   end
 
   def self.create_from_filename(name)
-    parse_song = name.split(" - ")[1]
-    parse_artist = name.split(" - ")[0]
-    parse_genre = name.split(" - ")[2].gsub(".mp3", "")
-    artist = Artist.find_or_create_by_name(parse_artist)
-    genre = Genre.find_or_create_by_name(parse_genre)
+    self.new_from_filename(name)
     self.create(parse_song, artist, genre)
 
   end
