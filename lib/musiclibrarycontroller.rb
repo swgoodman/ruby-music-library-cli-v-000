c0611c048908ce183eec95d62515f828059c88c7
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-15 23:25:51 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -41,6 +41,7 @@ class MusicLibraryController
     def list_songs_by_artist
       puts "Please enter the name of an artist:"
       input = gets.chomp
+      alpha = Song.all.collect {|song| song.artist = input}
     end
 
   end
