d3763cbd4952c7517e67cb657ee3d46556d09b71
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-15 22:08:39 +0000

    automatically backed up by learn

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -7,12 +7,12 @@ class MusicImporter
     @path = path
   end
 
-def files
-  @files ||= Dir.glob("#{path}/*.mp3").collect{|f| f.gsub("#{path}/", "")}
-end
+  def files
+    @files ||= Dir.glob("#{path}/*.mp3").collect{|f| f.gsub("#{path}/", "")}
+  end
 
-def import
-
-end
+  def import
+    @files.each { |song| song.create_from_filename}
+  end
 
 end
