a79158ee5a834488bb2953c71993dca27f9282f8
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-15 23:23:51 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -33,7 +33,7 @@ class MusicLibraryController
       alpha.each_with_index {|artist, index| puts "#{index+1}. #{artist.name}"}
     end
 
-    def genres
+    def list_genres
       alpha = Genre.all.sort_by! {|genre| genre.name}
       alpha.each_with_index {|genre, index| puts "#{index+1}. #{genre.name}"}
     end
