94d9a597d1fad4e6d22de5b40510aac87281abbe
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-16 00:00:15 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -42,7 +42,7 @@ class MusicLibraryController
     def list_songs_by_artist
       puts "Please enter the name of an artist:"
       specific_artist = gets.chomp
-        Artist.find_by_name(specific_artist).songs.each {|song| puts "#{song.artist.name} - #{song.name} - #{song.genre.name}"}
+        #Artist.find_by_name(specific_artist).songs.each {|song| puts "#{song.artist.name} - #{song.name} - #{song.genre.name}"}
     end
 
   end
