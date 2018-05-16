c93aff13b72f33170b16ea11bd414504216b1902
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-16 01:46:13 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -36,8 +36,7 @@ class MusicLibraryController
           list_songs_by_artist
         when "list genre"
           list_songs_by_genre
-        else
-          puts "invalid action"
+
     end
   end
 end
