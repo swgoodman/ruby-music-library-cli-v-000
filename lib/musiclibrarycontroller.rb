edb6b7a8011a23379b0abf6d1ae1847268f20fab
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-16 00:48:10 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -26,8 +26,8 @@ class MusicLibraryController
     end
 
     def list_songs
-      @alpha = Song.all.sort_by! {|song| song.name}
-      @alpha.each_with_index {|song, index| puts "#{index+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
+      alpha = Song.all.sort_by! {|song| song.name}
+      alpha.each_with_index {|song, index| puts "#{index+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
     end
 
     def list_artists
