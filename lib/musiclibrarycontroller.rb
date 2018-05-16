83591f6e46c342e435b18a3a631c2299a101f604
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-16 00:40:49 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -59,7 +59,9 @@ class MusicLibraryController
 
     def play_song
       puts "Which song number would you like to play?"
-      song = Song.all[gets.to_i - 1]
+      list_songs
+      song = list_songs[gets.to_i - 1]
+      
       puts "Playing #{song.name} by #{song.name.artist}"
     end
 
